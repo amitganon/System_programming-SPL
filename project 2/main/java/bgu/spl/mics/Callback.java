96531169920ba7f3bddff4f6a2a6c4f243bfed34
Amit Ganon <83574451+amitganon@users.noreply.github.com>
2022-03-09 14:27:45 +0200

    Delete project 2 directory

--- project 2/main/java/bgu/spl/mics/Callback.java
+++
@@ -1,8 +0,0 @@
-package bgu.spl.mics;
-
-/**
- * a callback is a function designed to be called when a message is received.
- */
-public interface Callback<T> {
-    public void call(T c);
-}
--- project 2/main/java/bgu/spl/mics/example/ServiceCreator.java
+++
@@ -1,7 +0,0 @@
-package bgu.spl.mics.example;
-
-import bgu.spl.mics.MicroService;
-
-public interface ServiceCreator {
-    MicroService create(String name, String[] args);
-}
